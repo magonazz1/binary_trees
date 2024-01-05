88d9deb74c7613a9dfb854cd57af230fe4481755
magonazz1 <martinmagonazz@outlook.com>
2024-01-05 14:16:15 -0800

    betty fix

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - function that measures the depth of a node in a binary tree
+ * binary_tree_depth - function that measures the depth of
+ *			a node in a binary tree
  *
  * @tree: Pointer to the root node of the tree to traverse
  *
